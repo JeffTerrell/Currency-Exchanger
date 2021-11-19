8105e24b5b89d52740a1d1688095090dd6cfaab4
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-19 12:34:14 -0500

    add base UI logic to index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRateService from "./js/exchangerate.js";
+
+
+function displayExchangeRate (response, usdNumber, currency) {
+
+}
+
 
 
 $(document).ready(function() {
-  
+  $('#currencyConvert').click(function() {
+    const usdNumber = $('#usdNumber').val();
+    const currency = $('currency').val();
+    $('#usdNumber').val("");
+    $('#currency').val("");
+
+    ExchangeRateService.getExchangeRate()
+      .then(function(response) {
+        if (response instanceof Error) {
+          throw Error(`ExchangeRate API error: ${response.message}`);
+        }
+        displayExchangeRate(response, usdNumber, currency);
+      });
+  });
 });
